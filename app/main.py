7d158f7698d6fb3314fa90a6b556f89de0bd440d
mkPuzon <118487929+mkPuzon@users.noreply.github.com>
2026-03-06 11:24:22 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,19 +1,28 @@
 import sys
-
+import shutil
+        
 def main():
+    builtin_commands = ["echo", "type", "exit"]
     while True:
         sys.stdout.write("$ ")
-
         command = input()
+
         if command == "exit":
             break
-        elif command[:4] == "type":
-            if command[5:] in ["echo", "type", "exit"]:
+
+        elif command.startswith("echo "):
+            print(command[5:])
+
+        elif command.startswith("type "):
+            cmd_arg = command[5:]
+            if cmd_arg in builtin_commands:
                 print(f"{command[5:]} is a shell builtin")
+            elif path := shutil.which(cmd_arg):
+                print(f"{cmd_arg} is {path}")
             else:
                 print(f"{command[5:]}: not found")
-        elif command[:4] == "echo":
-            print(command[5:])
+
+
         else:
             print(f"{command}: command not found")
 
