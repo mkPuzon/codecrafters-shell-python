fa87d503ff9ef7f6f37e7e6423844a7ffef83c61
mkPuzon <118487929+mkPuzon@users.noreply.github.com>
2026-03-06 09:52:37 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ def main():
             break
         elif command[:4] == "echo":
             print(command[5:])
-
-        print(f"{command}: command not found")
+        else:
+            print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
