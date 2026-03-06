4383778f79f267b564e52e42ac79be691be0188e
mkPuzon <118487929+mkPuzon@users.noreply.github.com>
2026-03-06 09:46:59 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 import sys
 
-
 def main():
-    sys.stdout.write("$ ")
-    command = input()
-    sys.stdout.write(f"{command}: command not found")
-
+    while True:
+        sys.stdout.write("$ ")
+        command = input()
+        sys.stdout.write(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
